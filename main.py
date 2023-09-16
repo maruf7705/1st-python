05c862bb212e1ab00ffc80b30703ffadea7fe297
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-17 00:07:23 +0600

    I am learing Pyhton
    I am Learning About Python

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-print("Hello world")
-a = 50 
-b = 50
-c = a+b 
-print(c)
+print("Maruf Islam \nSadik islam")
+# Lets go 2nd
+print("Maruf" + " Islam")
+# NEXT
+print("Maruf" + " " + "Islam")
+#................
+#Fix the code below 👇
+
+#TEST 2 --
+#TEST 2 --
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+#--------------------------------------
+
