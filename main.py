0835dc254c077e989999fd6cd8fc234c270d1f6d
MD. YEAMIN BIN JAMAL <113976731+yeaminbin@users.noreply.github.com>
2023-09-17 00:23:41 +0600

    Learn 3rd part
    Make a new part

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 #--------------------------------------
-
+#--------- INPUT ---------------------
+print("Hello" + " " + input(" What is your name ?"))
